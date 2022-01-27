62a3fb21f4cb038f9a2683452bbfc81e296d9596
AJ-Wi <tropaguararia28@gmail.com>
2022-01-26 21:00:30 -0500

    organizing folders

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -3,3 +3,24 @@ export const d = document;
 export const $ = (selector, context = d) => context.querySelector(selector);
 
 export const $$ = (selector, context = d) => context.querySelectorAll(selector);
+
+export function observer({ el, perc, toggleClass = false }) {
+  new IntersectionObserver(
+    ([e]) => {
+      if (toggleClass) {
+        el[1].classList.toggle(toggleClass, e.intersectionRatio < perc);
+      } else {
+        if (e.intersectionRatio > perc && moveItem === undefined) {
+          if (navbarStyle === "fixed") {
+            navMove(el[1], false);
+          } else {
+            navMove(el[1], true);
+          }
+        } else if (moveItem === "#" + el[0].id) {
+          moveItem = undefined;
+        }
+      }
+    },
+    { threshold: [perc] }
+  ).observe(el[0]);
+}
