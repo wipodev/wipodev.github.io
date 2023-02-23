c3790a20146c90838183c79adbc83cd25ddda103
wipodev <ajwipo@gmail.com>
2023-02-23 18:39:50 -0500

    deleted everything and start all over again with vitepress

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import AJWipo from "./lib/AJWipo.min.js";
-import "./app.js";
-//import "./store/index.js";
-
-const AJwipo = () => new AJWipo(".container").render("aj-wipo");
-
-document.addEventListener("DOMContentLoaded", AJwipo);
