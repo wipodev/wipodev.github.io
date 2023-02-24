af3f398743a0470ce680d196e1103f422613bc73
wipodev <ajwipo@gmail.com>
2023-02-23 20:15:35 -0500

    added links to featured projects

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -14,7 +14,17 @@ export default {
       label: "English",
       lan: "en",
       themeConfig: {
-        nav: [{ text: "Home", link: "/" }],
+        nav: [
+          { text: "Home", link: "/" },
+          {
+            text: "Projects",
+            items: [
+              { text: "Mark for Chat", link: "https://wipodev.com/markforchat/" },
+              { text: "Liquid Simulator", link: "https://www.wipodev.com/liquid-simulator-godot/" },
+              { text: "VuePress Theme", link: "https://www.wipodev.com/vuepress-theme-ajwi/" },
+            ],
+          },
+        ],
         footer: {
           message: "Released under the MIT License.",
           copyright: 'Copyright © 2023-present <a href="https://github.com/wipodev">WipoDev</a>',
@@ -26,7 +36,17 @@ export default {
       lan: "es",
       link: "/es/",
       themeConfig: {
-        nav: [{ text: "Inicio", link: "/es/" }],
+        nav: [
+          { text: "Inicio", link: "/es/" },
+          {
+            text: "Proyectos",
+            items: [
+              { text: "Marcar para chatear", link: "https://wipodev.com/markforchat/es/" },
+              { text: "Simulador de Líquidos", link: "https://www.wipodev.com/liquid-simulator-godot/es/" },
+              { text: "Tema VuePress", link: "https://www.wipodev.com/vuepress-theme-ajwi/es/" },
+            ],
+          },
+        ],
         footer: {
           message: "Publicado bajo la licencia MIT.",
           copyright: 'Copyright © 2023-presente <a href="https://github.com/wipodev">WipoDev</a>',
